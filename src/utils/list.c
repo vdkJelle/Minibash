a92d9faedd4cbbc019cddf4f8ad80b2cc91ba5e9
tevan-de <tevan-de@student.codam.nl>
2022-06-05 18:42:13 +0200

    NOTE: only for during development, needs to be removed for final product

--- src/utils/list.c
+++ src/utils/list.c
@@ -6,44 +6,11 @@
 /*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/24 11:46:52 by tevan-de      #+#    #+#                 */
-/*   Updated: 2022/06/01 14:25:09 by tevan-de      ########   odam.nl         */
+/*   Updated: 2022/06/03 14:16:24 by tessa         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-/*
-** Joins the word segments into a word
-** Returns a malloced string with the word
-*/
-
-// char	*join_word(t_word *arg)
-// {
-// 	char	*word;
-// 	int		size;
-// 	t_list	*temp;
-
-// 	temp = arg->word_segment;
-// 	size = 0;
-// 	while (temp)
-// 	{
-// 		size += ft_strlen(((char*)temp->content));
-// 		temp = temp->next;
-// 	}
-// 	word = malloc_guard(ft_calloc(size + 1, sizeof(char)));
-// 	temp = arg->word_segment;
-// 	while (temp)
-// 	{
-// 		ft_strlcat(word, (char*)temp->content, size + 1);
-// 		temp = temp->next;
-// 	}
-// 	return (word);
-// }
-
-/*
-** Prints the contents of the expression
-** No return value
-*/
 
 void	print_expression(void *content)
 {
