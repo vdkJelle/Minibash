cc88d612c7241417cac16ce625a4172e5772b6bf
tevan-de <tevan-de@student.codam.nl>
2022-06-07 18:43:05 +0200

    refactor: small adjustments to order of checking statements, norme: code is norme compliant, style: comments formatted

--- src/final_args.c
+++ src/final_args.c
@@ -6,19 +6,19 @@
 /*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/13 23:45:16 by tevan-de      #+#    #+#                 */
-/*   Updated: 2022/06/06 18:21:07 by tessa         ########   odam.nl         */
+/*   Updated: 2022/06/07 18:41:29 by tessa         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 /*
-** Gets the size of the new array
-** Assumes there is an argument after the redeirection
-**		arguments where checked for syntax errors in parser_check
-** Determines the original size of the array
-** Substracts 2 of the original size of the array if it spots a redirection
-** Returns an integer with the size of the new array
+**	Gets the size of the new array
+**	Assumes there is an argument after the redeirection arguments where ...
+**	... checked for syntax errors in parser_check
+**	Determines the original size of the array
+**	Substracts 2 of the original size of the array if it spots a redirection
+**	Returns an integer with the size of the new array
 */
 
 static int	get_new_size(t_word **arg)
@@ -45,13 +45,14 @@ static int	get_new_size(t_word **arg)
 }
 
 /*
-** Puts all information needed to execute the command into a null-terminated array of strings and handles redirections
-** Calls get_new_size to get the correct size of the new array
-** There can be none, one or multiple arguments
-** In case of a redirection
-**		redirection is called
-**		the redirection and next argument not added to the final array
-** No return value
+**	Puts all information needed to execute the command into a null-terminated ...
+**	... array of strings and handles redirections
+**	Calls get_new_size to get the correct size of the new array
+**	There can be none, one or multiple arguments
+**	In case of a redirection
+**		- redirection is called
+**		- the redirection and next argument are not added to the final array
+**	No return value
 */
 
 void	get_final_args_and_handle_redirections(t_data *data,
@@ -67,8 +68,8 @@ void	get_final_args_and_handle_redirections(t_data *data,
 	j = 0;
 	while (expression->arg && expression->arg[i])
 	{
-		if (is_redirection(expression->arg[i]->word)
-			&& expression->arg[i]->metacharacter)
+		if (expression->arg[i]->metacharacter
+			&& is_redirection(expression->arg[i]->word))
 		{
 			if (redirection(data, expression->arg, i, exec->fd))
 				return (free_array_part(&exec->args, j));
