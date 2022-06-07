721f1c2f1e0572391ad408681a8cb37ecc893cf6
tevan-de <tevan-de@student.codam.nl>
2022-06-07 18:30:33 +0200

    fix: prints correct error message in case close fails, norme: code is norme compliant, style: comments formatting

--- src/execute_builtin.c
+++ src/execute_builtin.c
@@ -6,15 +6,15 @@
 /*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/14 15:01:20 by tevan-de      #+#    #+#                 */
-/*   Updated: 2022/06/06 17:46:30 by tessa         ########   odam.nl         */
+/*   Updated: 2022/06/07 18:29:13 by tessa         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 /*
-** Calls the builtin function based on the command
-** No return value
+**	Calls the builtin function based on the command
+**	No return value
 */
 
 static void	execute_builtin(t_data *data, e_command cmd, t_execute *exec)
@@ -33,14 +33,14 @@ static void	execute_builtin(t_data *data, e_command cmd, t_execute *exec)
 }
 
 /*
-** Executes the builtin command when there is no pipe
-** If there is no redirection the 
-**		fd read is set to STDIN_FILENO
-**		fd write is set to STDOUT_FILENO
-** If there is a redirection the fds are set to the fd of the exec
-** Calls execute_builtin
-** Closes the fd after execute_builtin in case of a redirection
-** No return value
+**	Executes the builtin command when there is no pipe
+**	If there is no redirection the 
+**		- fd read is set to STDIN_FILENO
+**		- fd write is set to STDOUT_FILENO
+**	If there is a redirection the fds are set to the fd in the exec struct
+**	Calls execute_builtin
+**	Closes the fd after execute_builtin in case of a redirection
+**	No return value
 */
 
 void	execute_builtin_no_pipe(t_data *data, e_command cmd, t_execute *exec)
@@ -57,18 +57,19 @@ void	execute_builtin_no_pipe(t_data *data, e_command cmd, t_execute *exec)
 		ft_putstr_fd("exit\n", STDERR_FILENO);
 	execute_builtin(data, cmd, exec);
 	if (exec->fd[READ] != NO_REDIRECTION && close(exec->fd[READ]) == -1)
-		return (print_errno());
+		return (print_error(data, 1, make_array("🐶 > ", strerror(errno), NULL,
+					NULL)));
 	if (exec->fd[WRITE] != NO_REDIRECTION && close(exec->fd[WRITE]) == -1)
-		return (print_errno());
-
+		return (print_error(data, 1, make_array("🐶 > ", strerror(errno), NULL,
+					NULL)));
 }
 
 /*
-** Executes the builtin command in case of a pipe
-** The fd read is set to STDIN_FILENO and the fd write is set to STDOUT_FILENO
-** Calls execute_builtin
-** Without the syscall to exit the child process will not terminate normally
-** No return value
+**	Executes the builtin command in case of a pipe
+**	The fd read is set to STDIN_FILENO and the fd write is set to STDOUT_FILENO
+**	Calls execute_builtin
+**	The call to exit is necessary to exit the child process
+**	No return value
 */
 
 void	execute_builtin_pipe(t_data *data, e_command cmd, t_execute *exec)
